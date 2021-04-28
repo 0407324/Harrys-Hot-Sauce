13155bbdececdd425b23785dfd7e69b29257fa64
its.Magzzz <70177478+0407324@users.noreply.github.com>
2021-04-28 18:19:27 -0400

    Add files via upload

---
+++ Harrys hot sauce/Appendix_A_Connection.php
@@ -0,0 +1,12 @@
+<?PHP
+/* The connection to MySQL Database*/
+$db_host = "davyh4100.csd2800oo01c.21sp.stu.clarkstate.edu";
+// your user name
+$db_user = "davyh4100";
+// your password
+$db_pass = "ewSjJthbXI4NLcLb";
+// database name
+$db_dbname = "CONNECTION";
+// database port
+$db_port = "3306";
+?>
